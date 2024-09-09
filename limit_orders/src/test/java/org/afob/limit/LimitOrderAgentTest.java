5a02f79027ed1243b038ce5fa7ee28c74d35bdcd
Murugesh7354 <murugesh.t@in.ey.com>
2024-09-09 10:19:23 +0530

    Update LimitOrderAgentTest.java

--- limit_orders/src/test/java/org/afob/limit/LimitOrderAgentTest.java
+++ limit_orders/src/test/java/org/afob/limit/LimitOrderAgentTest.java
@@ -1,12 +1,59 @@
 package org.afob.limit;
 
-import org.junit.Assert;
+import org.afob.execution.ExecutionClient;
+import org.afob.execution.ExecutionClient.ExecutionException;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.*;
+
 public class LimitOrderAgentTest {
+    private ExecutionClient executionClient;
+    private LimitOrderAgent agent;
+
+    @Before
+    public void setUp() {
+        executionClient = mock(ExecutionClient.class);
+        agent = new LimitOrderAgent(executionClient);
+    }
 
     @Test
-    public void addTestsHere() {
-        Assert.fail("not implemented");
+    public void testBuyOrderExecutedWhenPriceIsBelowLimit() throws ExecutionException {
+        agent.addOrder(true, "IBM", 1000, new BigDecimal("100"));
+
+        agent.priceTick("IBM", new BigDecimal("99"));
+
+        verify(executionClient).buy("IBM", 1000);
+    }
+
+    @Test
+    public void testSellOrderExecutedWhenPriceIsAboveLimit() throws ExecutionException {
+        agent.addOrder(false, "IBM", 1000, new BigDecimal("100"));
+
+        agent.priceTick("IBM", new BigDecimal("101"));
+
+        verify(executionClient).sell("IBM", 1000);
+    }
+
+    @Test
+    public void testNoExecutionWhenPriceIsNotAtLimit() throws ExecutionException {
+        agent.addOrder(true, "IBM", 1000, new BigDecimal("100"));
+
+        agent.priceTick("IBM", new BigDecimal("101"));
+
+        verify(executionClient, never()).buy(anyString(), anyInt());
+        verify(executionClient, never()).sell(anyString(), anyInt());
+    }
+
+    @Test
+    public void testOrderNotExecutedTwice() throws ExecutionException {
+        agent.addOrder(true, "IBM", 1000, new BigDecimal("100"));
+
+        agent.priceTick("IBM", new BigDecimal("99"));
+        agent.priceTick("IBM", new BigDecimal("98"));
+
+        verify(executionClient, times(1)).buy("IBM", 1000);
     }
 }
